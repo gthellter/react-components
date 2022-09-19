9ffc4649cc28f61670e229d63f3d2c23cb13f1b2
Drew Henderson <drewsworld16@gmail.com>
2022-09-19 11:21:43 -0500

    added groceryList Item

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+const GroceryListItem = (props) => (
+
+  <li>{props.food}</li>
+);
+
+const GroceryList = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryListItem foods={['Pizza', 'Pinnapple', 'Ham']} />
+  </div>
+);
+
+
+// const Chicken = () => (
+//   <li>Chicken</li>
+// );
+// const Bread = () => (
+//   <li>Bread</li>
+// );
+
+//Models
+ReactDOM.render(<GroceryList />, document.querySelector("#app"))
